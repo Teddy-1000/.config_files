eaddcbb761d310d03e0ffd997996cc4533174810
amundis <amundis@student.matnat.uio.no>
2017-10-19 00:10:18 +0200

    Now with settup script

--- settup_scripts.sh
+++ settup_scripts.sh
@@ -8,15 +8,25 @@ apt install git
 
 git clone git@github.com:Mr-TeddyBear/.config_files.git ~/
 
+git clone https://github.com/sjl/badwolf.git ~/Downloads
+
+cp ~/Downloads/badwolf/colors
+
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 chsh -s /bin/zsh
 
 mkdir ~/.config/nvim/
 
+mkdir ~/.config/nvim/colors
+
+cp ~/Downloads/badwolf/colors/badwolf.vim ~/.config/nvim/colors/
+
 ln ~/.config_files/init.vim ~/.config/nvim/
 
 ln ~/.config_files/.zshrc ~/
+
+apt install python3
 
 apt install python3-pip
 
