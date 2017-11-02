e54334c1e31241ba96562dfc3df4a99fd3aa73cd
Mr-TeddyBear <amundis@student.matnat.uio.no>
2017-11-02 22:55:28 +0100

    Upadtedet scripts

--- config_files_install.sh
+++ config_files_install.sh
@@ -3,27 +3,34 @@
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-git clone https://github.com/sjl/badwolf.git ~/Downloads
+git clone https://github.com/sjl/badwolf.git ~/Downloads/badwolf
 
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 chsh -s /bin/zsh
 
-mkdir ~/.config/nvim/
+mkdir ~/.config/nvim/ -p
 
-mkdir ~/.config/nvim/colors
+mkdir ~/.config/nvim/colors -p
 
-cp ~/Downloads/badwolf/colors/badwolf.vim ~/.config/nvim/colors/
+cp ~/Downloads/colors/badwolf.vim ~/.config/nvim/colors/
+
+rm ~/.config/nvim/init.vim
 
 ln ~/.config_files/init.vim ~/.config/nvim/
 
+rm ~/.zshrc
+
 ln ~/.config_files/.zshrc ~/
+
+rm ~/.tmux.conf
 
 ln ~/.config_files/.tmux.conf ~/
 
+mkdir ~/Downloads/font 
 # clone
-git clone https://github.com/powerline/fonts.git --depth=1 ~/Downloads/
+git clone https://github.com/powerline/fonts.git --depth=1 ~/Downloads/font/
 # install
-./~/Downloads/fonts/install.sh
+bash ~/Downloads/font/install.sh
 # clean-up a bit
-rm -rf ~/Downloads/fonts
+rm -rf ~/Downloads/font
