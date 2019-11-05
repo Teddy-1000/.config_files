5250b4b1d02d2afff4bf75585cb33b3a55a9b792
MrTeddyBear <amundis@student.matnat.uio.no>
2019-11-05 11:46:49 +0100

    Add new spotify repo

--- program_downloads.sh
+++ program_downloads.sh
@@ -6,7 +6,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB31
 
 # 2. Add the Spotify repository
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-
 # 3. Update list of available packages
 sudo apt-get update
 
